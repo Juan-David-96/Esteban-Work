61fe12efda4c6aee7b63aa12e37e08d3e04a8d97
Juan_D <jdvc96@gmail.com>
2025-02-01 21:27:08 -0300

    Arreglo visual postulaciones y otros

--- app/models/postulacion.rb
+++ app/models/postulacion.rb
@@ -3,5 +3,12 @@ class Postulacion < ApplicationRecord
   belongs_to :post
 
   validates :fecha_postulacion, presence: true
-  validates :user_id, uniqueness: { scope: :post_id, message: "Ya se ha postulado a esta oferta." }
+  validate :unique_application
+
+  def unique_application
+    if Postulacion.exists?(user_id: user_id, post_id: post_id)
+      errors.add(:base, "Ya te has postulado a esta oferta.")
+    end
+  end
+
 end
